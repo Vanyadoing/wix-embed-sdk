d3071c557655f9927ab6c62a1b78f686ec0adbe8
Vanyadoing <51852870+Vanyadoing@users.noreply.github.com>
2025-06-15 18:25:29 +0300

    Fix build output path for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   build: {
+    lib: {
+      entry: 'src/main.ts',
+      formats: ['es'],
+      fileName: () => `main.js`,
+    },
     outDir: 'dist',
     rollupOptions: {
       input: 'index.html'
     }
   }
-});
+})
